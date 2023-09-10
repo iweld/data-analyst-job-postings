2ba8896384639209de28347e359b813c774a242e
Jaime M. Shaker <eyeweld@gmail.com>
2023-09-10 09:51:15 -0500

    Reordering temp table and adding questions #4 and #5

--- source_data/scripts/sql_analysis.sql
+++ source_data/scripts/sql_analysis.sql
@@ -15,10 +15,15 @@
 
 -- 1. How many records does this table contain?
 
-SELECT
-	count(*) AS record_count
-FROM
-	data_analyst.jobs;
+DROP TABLE IF EXISTS get_record_count;
+CREATE TEMP TABLE get_record_count AS (
+	SELECT
+		count(*) AS record_count
+	FROM
+		data_analyst.jobs
+);
+
+SELECT * FROM get_record_count;
 
 -- Results:
 
@@ -60,46 +65,131 @@ data_job_id|idx |title                                                          
        
  */
 
--- 3. List basic salary statistics (mean, min, median...) for hourly rates and the specific shedule type.
+-- 3. How many records do not have any salary information and what is the percentage of records that do not have any salary information?
 
+WITH get_totals_cte AS (
+	SELECT
+		(SELECT * FROM get_record_count) AS total_records,
+		count(*) AS no_salary_count
+	FROM
+		data_analyst.jobs
+	WHERE
+		salary_standardized IS NULL
+)
 SELECT
-	COALESCE(schedule_type, 'unknown') AS hourly_rate_schedule_type,
-	count(*) AS number_of_jobs,
-	round(min(salary_min)::NUMERIC, 2) AS min_salary,
-	round(avg(salary_hourly)::NUMERIC, 2) AS avg_salary,
-	round(percentile_cont(0.25) WITHIN GROUP (ORDER BY salary_hourly)::NUMERIC, 2) AS perc_25_salary,
-	round(percentile_cont(0.5) WITHIN GROUP (ORDER BY salary_hourly)::NUMERIC, 2) AS median_salary,
-	round(percentile_cont(0.75) WITHIN GROUP (ORDER BY salary_hourly)::NUMERIC, 2) AS perc_75_salary,
-	round(MODE() WITHIN GROUP (ORDER BY salary_hourly)::NUMERIC, 2) AS mode_salary,
-	round(max(salary_max)::NUMERIC, 2) AS max_salary
+	total_records AS total_record_count,
+	no_salary_count,
+	total_records - no_salary_count AS record_diff,
+	round(100 * no_salary_count::numeric / total_records, 2) AS no_salary_percentage
 FROM
-	data_analyst.jobs
-WHERE
-	salary_hourly IS NOT NULL
-GROUP BY
-	schedule_type;
+	get_totals_cte;
 
 -- Results:
 
 /*
 
-hourly_rate_schedule_type           |number_of_jobs|min_salary|avg_salary|perc_25_salary|median_salary|perc_75_salary|mode_salary|max_salary|
-------------------------------------+--------------+----------+----------+--------------+-------------+--------------+-----------+----------+
-Contractor                          |          2625|      8.00|     43.89|         29.00|        39.90|         57.50|      57.50|    500.00|
-Contractor and Temp work            |            14|     12.00|     49.43|         27.25|        40.00|         59.00|      59.00|    180.00|
-Full-time                           |           509|     10.00|     41.34|         23.27|        36.00|         57.50|      23.27|    150.00|
-Full-time and Part-time             |             1|     18.33|     23.27|         23.27|        23.27|         23.27|      23.27|     28.20|
-Full-time, Part-time, and Internship|             1|     18.46|     22.70|         22.70|        22.70|         22.70|      22.70|     26.93|
-Internship                          |             3|     17.00|     22.17|         21.75|        22.50|         22.75|      21.00|     28.00|
-Part-time                           |            50|     10.00|     43.27|         25.00|        31.50|         56.13|      30.00|    150.00|
-                                    |             6|     25.00|     57.46|         57.95|        62.50|         66.88|      67.50|     75.00|
+total_record_count|no_salary_count|record_diff|no_salary_percentage|
+------------------+---------------+-----------+--------------------+
+             28375|          23255|       5120|               81.96|
+   
+ */
+
+-- 4. List basic salary statistics (mean, min, median...) for hourly rates and the specific shedule type.
+
+WITH get_hourly_stats AS (
+	SELECT
+		CASE
+			WHEN schedule_type = 'Contractor and Temp work' THEN 'Contractor'
+			WHEN schedule_type = 'Full-time and Part-time' OR schedule_type = 'Full-time, Part-time, and Internship' THEN 'Full-time'
+			WHEN schedule_type IS NULL THEN 'Uknown'
+			ELSE schedule_type
+		END AS hourly_rate_schedule_type,
+		count(*) AS number_of_jobs,
+		round(min(salary_hourly)::NUMERIC, 2) AS hourly_min,
+		round(avg(salary_hourly)::NUMERIC, 2) AS hourly_avg,
+		round(percentile_cont(0.25) WITHIN GROUP (ORDER BY salary_hourly)::NUMERIC, 2) AS hourly_25_perc,
+		round(percentile_cont(0.5) WITHIN GROUP (ORDER BY salary_hourly)::NUMERIC, 2) AS hourly_median,
+		round(percentile_cont(0.75) WITHIN GROUP (ORDER BY salary_hourly)::NUMERIC, 2) AS hourly_75_perc,
+		round(MODE() WITHIN GROUP (ORDER BY salary_hourly)::NUMERIC, 2) AS hourly_mode,
+		round(max(salary_hourly)::NUMERIC, 2) AS hourly_max
+	FROM
+		data_analyst.jobs
+	WHERE
+		salary_hourly IS NOT NULL
+	GROUP BY
+		hourly_rate_schedule_type
+)
+SELECT
+	hourly_rate_schedule_type,
+	number_of_jobs,
+	cast(hourly_min AS money) AS hourly_min,
+	cast(hourly_avg AS money) AS hourly_min,
+	cast(hourly_25_perc AS money) AS hourly_25_perc,
+	cast(hourly_median AS money) AS hourly_median,
+	cast(hourly_75_perc AS money) AS hourly_75_perc,
+	cast(hourly_mode AS money) AS hourly_mode,
+	cast(hourly_max AS money) AS hourly_max
+FROM
+	get_hourly_stats;
+
+-- Results:
+
+/*
+
+hourly_rate_schedule_type|number_of_jobs|hourly_min|hourly_min|hourly_25_perc|hourly_median|hourly_75_perc|hourly_mode|hourly_max|
+-------------------------+--------------+----------+----------+--------------+-------------+--------------+-----------+----------+
+Contractor               |          2639|     $9.00|    $43.92|        $29.00|       $39.90|        $57.50|     $57.50|   $300.00|
+Full-time                |           511|    $11.00|    $41.27|        $23.27|       $35.50|        $57.50|     $23.27|   $125.00|
+Internship               |             3|    $21.00|    $22.17|        $21.75|       $22.50|        $22.75|     $21.00|    $23.00|
+Part-time                |            50|    $12.00|    $43.27|        $25.00|       $31.50|        $56.13|     $30.00|   $112.50|
+Uknown                   |             6|    $27.50|    $57.46|        $57.95|       $62.50|        $66.88|     $67.50|    $67.50|
        
  */
 
+-- 5. List basic salary statistics (mean, min, median...) for yearly rates and the specific shedule type.
 
+WITH get_yearly_stats AS (
+	SELECT
+		COALESCE(schedule_type, 'Unknown') AS yearly_rate_schedule_type,
+		count(*) AS number_of_jobs,
+		round(min(salary_yearly)::NUMERIC, 2) AS yearly_min,
+		round(avg(salary_yearly)::NUMERIC, 2) AS yearly_avg,
+		round(percentile_cont(0.25) WITHIN GROUP (ORDER BY salary_yearly)::NUMERIC, 2) AS yearly_25_perc,
+		round(percentile_cont(0.5) WITHIN GROUP (ORDER BY salary_yearly)::NUMERIC, 2) AS yearly_median,
+		round(percentile_cont(0.75) WITHIN GROUP (ORDER BY salary_yearly)::NUMERIC, 2) AS yearly_75_perc,
+		round(MODE() WITHIN GROUP (ORDER BY salary_yearly)::NUMERIC, 2) AS yearly_mode,
+		round(max(salary_yearly)::NUMERIC, 2) AS yearly_max
+	FROM
+		data_analyst.jobs
+	WHERE
+		salary_yearly IS NOT NULL
+	GROUP BY
+		schedule_type
+)
+SELECT
+	yearly_rate_schedule_type,
+	number_of_jobs,
+	cast(yearly_min AS money) AS yearly_min,
+	cast(yearly_avg AS money) AS yearly_min,
+	cast(yearly_25_perc AS money) AS yearly_25_perc,
+	cast(yearly_median AS money) AS yearly_median,
+	cast(yearly_75_perc AS money) AS yearly_75_perc,
+	cast(yearly_mode AS money) AS yearly_mode,
+	cast(yearly_max AS money) AS yearly_max
+FROM
+	get_yearly_stats;
 
+-- Results:
 
+/*
 
+yearly_rate_schedule_type|number_of_jobs|yearly_min|yearly_min |yearly_25_perc|yearly_median|yearly_75_perc|yearly_mode|yearly_max |
+-------------------------+--------------+----------+-----------+--------------+-------------+--------------+-----------+-----------+
+Contractor               |            51|$43,000.00| $96,318.82|    $75,250.00|   $90,000.00|   $110,000.00| $72,500.00|$175,000.00|
+Full-time                |          1847|$29,289.84|$101,582.29|    $85,000.00|   $96,500.00|   $112,500.00| $96,500.00|$233,500.00|
+Part-time                |             4|$37,300.00| $79,450.00|    $76,825.00|   $90,000.00|    $92,625.00| $90,000.00|$100,500.00|
+       
+ */
 
 
 
